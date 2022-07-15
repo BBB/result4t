49f75f2db451accaabf8b7e93dba7461461f64e4
Ollie Relph <ollie@relph.me>
2022-07-15 18:15:07 +0100

    Test fold

--- test/Either.ts
+++ test/Either.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { Either } from "../src/Either";
 
 const reverseString = (a: string) => a.split("").reverse().join("");
+const toString = <T extends { toString(): string }>(a: T) => a.toString();
 
 describe("right", () => {
   it("should contain a value", () => {
@@ -38,5 +39,22 @@ describe("mapLeft", () => {
   it("should not transform a value on the right", () => {
     const result = Either.right<string, string>("right").mapLeft(reverseString);
     expect(result.flatten()).toEqual("right");
+  });
+});
+
+describe("fold", () => {
+  it("should transform a value on the left", () => {
+    const result = Either.left<string, number>("left").fold(
+      reverseString,
+      toString
+    );
+    expect(result).toEqual("tfel");
+  });
+  it("should transform a value on the right", () => {
+    const result = Either.right<number, string>("right").fold(
+      toString,
+      reverseString
+    );
+    expect(result).toEqual("thgir");
   });
 });
