0eb74dd5611f965a22cf5306dcf3b701fa310b35
Ollie Relph <ollie@relph.me>
2022-08-01 19:49:17 +0100

    RunThrowLeft

--- src/Task.ts
+++ src/Task.ts
@@ -2,7 +2,7 @@ import { Either } from "./Either";
 
 type TaskEither<L, R> = () => Promise<Either<L, R>>;
 
-export class Task<L = never, R = never> {
+export class Task<L = never, R = never> implements PromiseLike<Either<L, R>> {
   constructor(private inner: TaskEither<L, R>) {}
 
   map<R2 = never>(param: (a: R) => R2): Task<L, R2> {
@@ -15,5 +15,25 @@ export class Task<L = never, R = never> {
 
   run() {
     return this.inner();
+  }
+
+  async runThrowLeft() {
+    const result = await this.run();
+    return result.getOrElse((err) => {
+      throw err;
+    });
+  }
+
+  then<TResult1 = Either<L, R>, TResult2 = never>(
+    onfulfilled?:
+      | ((value: Either<L, R>) => PromiseLike<TResult1> | TResult1)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: any) => PromiseLike<TResult2> | TResult2)
+      | undefined
+      | null
+  ): PromiseLike<TResult1 | TResult2> {
+    return this.run().then(onfulfilled, onrejected);
   }
 }
--- test/Task.ts
+++ test/Task.ts
@@ -12,6 +12,7 @@ describe("map", () => {
     expect(a).toEqual(Either.right(true));
   });
 });
+
 describe("mapLeft", () => {
   it("should transform a left value", async () => {
     class BooError extends Error {
@@ -25,5 +26,35 @@ describe("mapLeft", () => {
       .run();
 
     expect(a).toEqual(Either.left(booError));
+  });
+});
+
+describe("PromiseLike", () => {
+  it("should return the inner Either when awaited", async () => {
+    class BooError extends Error {
+      name = "BooError";
+    }
+    const booError = new BooError();
+    const a = await new Task(() =>
+      Promise.resolve(Either.left(new Error("Boo")))
+    ).mapLeft((val) => booError);
+
+    expect(a).toEqual(Either.left(booError));
+  });
+});
+describe("runThrowLeft", () => {
+  it("should return the right side", async () => {
+    await expect(
+      new Task(() => Promise.resolve(Either.right(true))).runThrowLeft()
+    ).resolves.toEqual(true);
+  });
+  it("should throw the left side", async () => {
+    class BooError extends Error {
+      name = "BooError";
+    }
+    const booError = new BooError();
+    await expect(
+      new Task(() => Promise.resolve(Either.left(booError))).runThrowLeft()
+    ).rejects.toEqual(booError);
   });
 });
