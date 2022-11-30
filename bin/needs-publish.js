5e86659e2a5811567b25c4ffbde5b71aad8f4609
Ollie Relph <ollie@relph.me>
2022-11-30 18:07:40 +0000

    Remove debug

--- bin/needs-publish.js
+++ bin/needs-publish.js
@@ -1,14 +1,7 @@
 const needsPublish = (packageName, version) => {
-  console.log({ packageName, version });
   return fetch(`https://registry.npmjs.org/${encodeURIComponent(packageName)}`)
-    .then((res) => {
-      console.log(res.status);
-      return res.ok ? res.json() : {};
-    })
-    .then((response) => {
-      console.log({ response });
-      return !(version in response.time);
-    })
+    .then((res) => (res.ok ? res.json() : {}))
+    .then((response) => !(version in response.time))
     .catch(() => true);
 };
 
