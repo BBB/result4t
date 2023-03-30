6e8adba5d3b1153be47bfc81a089f092b15c167d
Ollie Relph <ollie@relph.me>
2023-03-30 19:52:48 +0100

    test the statics

--- test/TaskResult.ts
+++ test/TaskResult.ts
@@ -2,78 +2,102 @@ import { describe, expect, it } from "vitest";
 import { Result } from "../src/Result";
 import { TaskResult } from "../src/TaskResult";
 
-describe("fromResult", () => {
-  it("will map to a TaskResult", async () => {
-    const out = await TaskResult.fromResult(Result.success(1))
-      .map(() => "hello")
-      .map(() => true);
+describe("static", () => {
+  describe("fromResult", () => {
+    it("will map to a TaskResult", async () => {
+      const out = await TaskResult.fromResult(Result.success(1))
+        .map(() => "hello")
+        .map(() => true);
 
-    expect(out).toStrictEqual(Result.success(true));
+      expect(out).toStrictEqual(Result.success(true));
+    });
+    it("will map to a TaskResult failure", async () => {
+      const out = await TaskResult.fromResult(Result.failure(1))
+        .map(() => "hello")
+        .map(() => true);
+
+      expect(out).toStrictEqual(Result.failure(1));
+    });
   });
-  it("will map to a TaskResult failure", async () => {
-    const out = await TaskResult.fromResult(Result.failure(1))
-      .map(() => "hello")
-      .map(() => true);
+  describe("fromPromise", () => {
+    it("will convert the Promise<T> to a TaskResult<T>", async () => {
+      const out = await TaskResult.fromPromise(
+        () => Promise.resolve(1),
+        (err) => new (class Failed {})()
+      ).map((int) => int + 1);
 
-    expect(out).toStrictEqual(Result.failure(1));
-  });
-});
-describe("map", () => {
-  it("should transform a success", async () => {
-    const out = await TaskResult.success(1)
-      .map(() => "hello")
-      .map(() => true);
+      expect(out).toStrictEqual(Result.success(2));
+    });
 
-    expect(out).toStrictEqual(Result.success(true));
-  });
-});
+    it("will convert the error to TaskResult<never,E>", async () => {
+      class Failed {}
+      const out = await TaskResult.fromPromise(
+        () => Promise.reject(1),
+        (err) => new Failed()
+      ).map((int) => int + 1);
 
-describe("mapFailure", () => {
-  it("should transform a failure", async () => {
-    class BooError extends Error {
-      name = "BooError";
-    }
-    const booError = new BooError();
-    const out = await TaskResult.failure(new Error("Boo")).mapFailure(
-      () => booError
-    );
-
-    expect(out).toStrictEqual(Result.failure(booError));
-  });
-});
-
-describe("PromiseLike", () => {
-  it("should return a Result when awaited", async () => {
-    class BooError extends Error {
-      name = "BooError";
-    }
-    const booError = new BooError();
-    const out = await TaskResult.failure(new Error("Boo")).mapFailure(
-      () => booError
-    );
-
-    expect(out).toStrictEqual(Result.failure(booError));
-  });
-
-  it("should pass a Result to a then", async () => {
-    const out = await TaskResult.success(true).then((a) =>
-      a.getOrElse(() => "woo")
-    );
-    expect(out).toStrictEqual(true);
+      expect(out).toStrictEqual(Result.failure(new Failed()));
+    });
   });
 });
+describe("instance", () => {
+  describe("map", () => {
+    it("should transform a success", async () => {
+      const out = await TaskResult.success(1)
+        .map(() => "hello")
+        .map(() => true);
 
-describe("runThrowFailure", () => {
-  it("should return the success", async () => {
-    const out = TaskResult.success(true).runThrowFailure();
-    await expect(out).resolves.toEqual(true);
+      expect(out).toStrictEqual(Result.success(true));
+    });
   });
-  it("should throw the failure", async () => {
-    class BooError extends Error {
-      name = "BooError";
-    }
-    const booError = new BooError();
-    const out = TaskResult.failure(booError).runThrowFailure();
-    await expect(out).rejects.toStrictEqual(booError);
+
+  describe("mapFailure", () => {
+    it("should transform a failure", async () => {
+      class BooError {
+        name = "BooError";
+      }
+      const booError = new BooError();
+      const out = await TaskResult.failure(new Error("Boo")).mapFailure(
+        () => booError
+      );
+
+      expect(out).toStrictEqual(Result.failure(booError));
+    });
+  });
+
+  describe("PromiseLike", () => {
+    it("should return a Result when awaited", async () => {
+      class BooError extends Error {
+        name = "BooError";
+      }
+      const booError = new BooError();
+      const out = await TaskResult.failure(new Error("Boo")).mapFailure(
+        () => booError
+      );
+
+      expect(out).toStrictEqual(Result.failure(booError));
+    });
+
+    it("should pass a Result to a then", async () => {
+      const out = await TaskResult.success(true).then((a) =>
+        a.getOrElse(() => "woo")
+      );
+      expect(out).toStrictEqual(true);
+    });
+  });
+
+  describe("runThrowFailure", () => {
+    it("should return the success", async () => {
+      const out = TaskResult.success(true).runThrowFailure();
+      await expect(out).resolves.toEqual(true);
+    });
+    it("should throw the failure", async () => {
+      class BooError extends Error {
+        name = "BooError";
+      }
+      const booError = new BooError();
+      const out = TaskResult.failure(booError).runThrowFailure();
+      await expect(out).rejects.toStrictEqual(booError);
+    });
   });
 });
