946c24dcc220aa14ab6149a2280c462170f74d23
Ollie Relph <ollie@relph.me>
2023-06-06 10:18:32 +0100

    Add docs

---
+++ docs/docs/intro.md
@@ -0,0 +1,14 @@
+---
+id: intro
+title: Quick setup guide
+slug: /
+tags:
+  - Guide
+  - First launch
+  - Getting started
+sidebar_position: 1
+---
+
+## Hello
+
+Wooo
---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      title={`Hello`}
+      description="Description will go into a meta tag in <head />"
+    ></Layout>
+  );
+}
