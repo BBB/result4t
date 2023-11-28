4d3da884678d4a41946e989cf19f150b938b0af7
Ollie Relph <ollie@relph.me>
2023-11-28 20:31:40 +0000

    Release 0.5.1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Option, optional, None, Some } from "./Option.js";
 export { Result, Success, Failure } from "./Result.js";
 export { TaskResult } from "./TaskResult.js";
+export { Task } from "./Task.js";
