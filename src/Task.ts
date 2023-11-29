1acdcae3c85597cbaa9dc29d72dae933dd75a6f0
Ollie Relph <ollie@relph.me>
2023-11-29 20:23:47 +0000

    define in terms of Return rather than Promise<Return>

--- src/Task.ts
+++ src/Task.ts
@@ -1,35 +1,29 @@
 export class Task<Args extends any[], Return> {
-  protected constructor(private fn: (...args: Args) => Promise<Return>) {}
+  protected constructor(private fn: (...args: Args) => Return) {}
 
   static of<
-    Fn extends (...args: any[]) => Promise<any>,
-    Args extends any[] = Fn extends (...args: infer R) => Promise<any>
-      ? R
-      : never,
-    Return = Fn extends (...args: any[]) => Promise<infer R> ? R : never,
+    Fn extends (...args: any[]) => any,
+    Args extends any[] = Fn extends (...args: infer R) => any ? R : never,
+    Return = Fn extends (...args: any[]) => infer R ? R : never,
   >(fn: Fn) {
     return new Task<Args, Return>(fn);
   }
 
-  map<Fn2 extends (...args: any) => Promise<any>>(
-    predicate: (fn: (...args: Args) => Promise<Return>) => Fn2,
+  map<Fn2 extends (...args: any) => any>(
+    predicate: (fn: (...args: Args) => Return) => Fn2,
   ) {
     return Task.of(predicate(this.fn));
   }
 
   flatMap<
     Fn2 extends (...args: any) => Task<any, any>,
-    Args2 extends any[] = Fn2 extends (...args: infer R) => Promise<any>
-      ? R
-      : never,
+    Args2 extends any[] = Fn2 extends (...args: infer R) => any ? R : never,
     Return2 = Fn2 extends (...args: any[]) => infer R ? R : never,
-  >(
-    predicate: (fn: (...args: Args) => Promise<Return>) => Task<Args2, Return2>,
-  ) {
+  >(predicate: (fn: (...args: Args) => Return) => Task<Args2, Return2>) {
     return predicate(this.fn);
   }
 
-  call(...args: Args): Promise<Return> {
+  call(...args: Args): Return {
     return this.fn(...args);
   }
 }
--- test/Task.ts
+++ test/Task.ts
@@ -1,17 +1,6 @@
 import { describe, expect, it } from "vitest";
-import utils from "node:util";
-import { Failure, Result, Success } from "../src/Result.js";
-import { TaskResult } from "../src/TaskResult.js";
 import { Task } from "../src/Task.js";
-
-class Failed {}
-
-const sleep = (ms: number) =>
-  TaskResult.fromPromise(
-    () => utils.promisify(setTimeout)(ms),
-    () => new Error("never"),
-  );
-
+1;
 describe("instance", () => {
   describe("map", () => {
     it("should allow for wrapping a value", async () => {
